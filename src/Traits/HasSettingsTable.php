cf88806683b8e41be64960e4901fff1f92652ac3
wanderingDaniel <dan@bookingninja.io>
2025-07-10 19:33:49 +0100

    Add Memo Driver

--- src/Traits/HasSettingsTable.php
+++ src/Traits/HasSettingsTable.php
@@ -34,7 +34,7 @@ trait HasSettingsTable
     public function getSettingsValue(): array
     {
         if (config('model_settings.settings_table_use_cache')) {
-            return Cache::rememberForever($this->getSettingsCacheKey(), function () {
+            return Cache::memo()->rememberForever($this->getSettingsCacheKey(), function () {
                 return $this->__getSettingsValue();
             });
         }
